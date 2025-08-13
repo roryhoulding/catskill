ae8332e386f73586a461bbdd301ea7926634fa2f
Rory Houlding <rohoulding@googlemail.com>
2025-08-13 17:28:00 -0400

    Create Zillow client with search by URL method

--- services/helloWorld.ts
+++ services/helloWorld.ts
@@ -1,3 +1,12 @@
-export const helloWorld = () => {
-    console.log("hello, world");
-}
+import { zillowApi } from "../clients/zillowApi";
+
+const hudsonValleyOneDaySearchUrl =
+  "https://www.zillow.com/homes/for_sale/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22isMapVisible%22%3Atrue%2C%22mapBounds%22%3A%7B%22west%22%3A-75.9885441015625%2C%22east%22%3A-72.1982608984375%2C%22south%22%3A40.81737559955039%2C%22north%22%3A42.98800528029415%7D%2C%22customRegionId%22%3A%226327468838X1-CRow3wi1ata20h_12tnev%22%2C%22filterState%22%3A%7B%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22doz%22%3A%7B%22value%22%3A%221%22%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A8%2C%22usersSearchTerm%22%3A%22%22%7D";
+
+export const helloWorld = async () => {
+  console.log("Fetching data from Zillow...");
+  const data = await zillowApi.getListingsBySearchUrl(
+    hudsonValleyOneDaySearchUrl,
+  );
+  console.log(data);
+};
