8a8da095806a91ff26b298a3e434f5fc05f0c4c1
Rory Houlding <rohoulding@googlemail.com>
2025-08-25 09:55:49 -0400

    Change back to proper url

--- jobs/hudsonValleyListings.ts
+++ jobs/hudsonValleyListings.ts
@@ -7,8 +7,7 @@ import { ListingContent } from "../types";
 async function main(): Promise<void> {
   try {
     const qualifiedListings = await qualifyListings(
-      // Config.hudsonValleySearchUrl,
-      "https://www.zillow.com/homes/for_sale/?searchQueryState=%7B%22isMapVisible%22%3Atrue%2C%22mapBounds%22%3A%7B%22west%22%3A-74.78129482076466%2C%22east%22%3A-74.662848470667%2C%22south%22%3A41.78949673150471%2C%22north%22%3A41.85742360935052%7D%2C%22filterState%22%3A%7B%22sort%22%3A%7B%22value%22%3A%22days%22%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22price%22%3A%7B%22min%22%3A0%2C%22max%22%3A2000000%7D%2C%22mp%22%3A%7B%22min%22%3A0%2C%22max%22%3A10091%7D%2C%22land%22%3A%7B%22value%22%3Afalse%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22%3Afalse%7D%2C%22doz%22%3A%7B%22value%22%3A%2230%22%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A13%2C%22customRegionId%22%3A%22462a168ab4X1-CRz5aie3hqao9x_w41n3%22%7D",
+      Config.hudsonValleySearchUrl,
     );
 
     const listingsContent: ListingContent[] = await Promise.all(
