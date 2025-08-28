a224bec37f5dd7b3353b1f00a67f203a5344c4d5
Rory Houlding <rohoulding@googlemail.com>
2025-08-28 12:23:21 -0400

    Make resuable

--- jobs/hudsonValleyListings.ts
+++ jobs/hudsonValleyListings.ts
@@ -1,5 +1,5 @@
 import { Config } from "../services/config";
 import { listingsParentProcessor } from "../services/listingsParentProcessor";
-import { promptInput } from "../prompts/qualifyListing/v3";
+import { promptInput } from "../prompts/qualifyListing/hudsonValley/v3-1";
 
 listingsParentProcessor(Config.hudsonValleySearchUrl, promptInput);
